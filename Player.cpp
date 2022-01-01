bd401015ac657cec36803f5a8bd020cc6a360a96
nitaiklu <nitai.kluger@gmail.com>
2022-01-01 22:40:59 +0200

    Player

---
+++ Player.cpp
@@ -0,0 +1,44 @@
+#include "Player.h"
+
+int Player::getLevel() const
+{
+    return level;
+}
+
+int Player::getGroup()
+{
+    return group;
+}
+
+int Player::getId() const
+{
+    return id;
+}
+
+void Player::setLevel(int new_level)
+{
+    level = new_level;
+}
+
+void Player::setGroup(int new_group)
+{
+    group = new_group;
+}
+
+void Player::printPlayer() const
+{
+    cout << "PLAYER:" << endl;
+    cout << "id = " << id << endl;
+    cout << "level = " << level << endl;
+    cout << "group = " << group << endl;
+}
+
+bool Player::operator==(const Player &compare) const
+{
+    return id == compare.id;
+}
+
+bool Player::operator!=(const Player &compare) const
+{
+    return !(*this == compare);
+}
---
+++ Player.h
@@ -0,0 +1,30 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+#include <iostream>
+#include <memory>
+
+using std::cout;
+using std::endl;
+
+class Player
+{
+private:
+    int id;
+    int level;
+    int group;
+
+public:
+    Player(int id, int level, int group) : id(id), level(level), group(group) {}
+    Player(const Player &copy) = default;
+    ~Player() = default;
+    int getId() const;
+    int getLevel() const;
+    int getGroup();
+    void setGroup(int new_group);
+    void setLevel(int new_level);
+    void printPlayer() const;
+    bool operator==(const Player &compare) const;
+    bool operator!=(const Player &compare) const;
+};
+#endif // PLAYER_H_
